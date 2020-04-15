f31995452fbb956a90d0bcb805756d91b41574ab
dfounderliu <dfounderliu@tencent.com>
2020-04-15 11:57:25 +0800

    fix: package-version

--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,7 @@
-'use strict';
 exports.main_handler = async (event, context, callback) => {
-  console.log("Hello World")
+  console.log('Hello World For Serverless aaa')
   console.log(event)
-  console.log(event["non-exist"])
+  console.log(event['non-exist'])
   console.log(context)
   return event
-};
+}
