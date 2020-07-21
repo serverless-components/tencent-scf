d6fbfe7e63143d4f25dd316a15a4e04de99a54e5
yugasun <yuga_sun@163.com>
2020-07-21 15:47:56 +0800

    fix: upgrade deps

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,6 @@
 exports.main_handler = async (event, context) => {
-  console.log('Hello World')
   console.log(event)
-  console.log(event['non-exist'])
-  console.log(context)
-  return event
+  return {
+    msg: 'Hello Serverless'
+  }
 }
